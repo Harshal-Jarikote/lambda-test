7b54cadc665b99cb625cc70a3004a8a12cc3fa38
Harshal Jarikote <jarikote.harshal@gmail.com>
2017-12-21 14:47:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ alert(1);
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("America/Los_Angeles");
+    currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: ' + currentTime.toString(),
